af1f0c1ce9a81cf06ca18301ab240e3b720e11ad
Bhuwan Bhatt <bhattbhuwan13@outlook.com>
2022-08-11 12:11:41 +0545

    added logic for too many transactions per day

--- fraud_detect.py
+++ fraud_detect.py
@@ -18,20 +18,40 @@ def load_json(file_name="mydata.json"):
             yield row["id"], value  # Change the form of value to key, value
 
 
+def current_transaction_amount_abnormally_higher(
+    current_amount, previous_amount
+):
+    return current_amount >= 1.5 * previous_amount
+
+
 class FraudTransaction:
     def __init__(self):
         self.previous_transaction_value = float("inf")
         self.current_transaction_value = None
         self.flagged_items = []
+        self.num_of_one_day_transactions = 1
+        self.previous_transaction_date = None
 
     def detect_fraud(self, data):
 
         self.current_transaction_value = data[1]
-        if (
-            self.current_transaction_value
-            >= 1.5 * self.previous_transaction_value
+
+        if current_transaction_amount_abnormally_higher(
+            self.current_transaction_value, self.previous_transaction_value
         ):
             self.flagged_items.append(data)
+
+        current_transaction_date = data[0]
+
+        if current_transaction_date == self.previous_transaction_date:
+            self.num_of_one_day_transactions += 1
+        else:
+            self.previous_transaction_date = current_transaction_date
+            self.num_of_one_day_transactions = 1
+
+        if self.num_of_one_day_transactions >= 3:
+            self.flagged_items.append(data)
+
         self.previous_transaction_value = self.current_transaction_value
         return self, self.flagged_items
 
